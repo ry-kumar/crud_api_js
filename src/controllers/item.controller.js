835483208d32a480fd6bf67d5b1b2de4ed1177db
YASWANTH <yaswanthrayana91221@yahoo.com>
2026-01-25 22:18:34 -0500

    Convert controller methods to async/await
    Refactor controller methods to use async/await for better handling of asynchronous operations.

--- src/controllers/item.controller.js
+++ src/controllers/item.controller.js
@@ -1,50 +1,51 @@
 const service = require('../services/item.service');
 const { createItemSchema } = require('../validators/item.schema');
 
-
-exports.create = (req, res, next) => {
+exports.create = async (req, res, next) => {
   try {
     const validatedData = createItemSchema.parse(req.body);
-    const item = service.create(validatedData);
-    res.status(201).json(item);
+    const item = await service.create(validatedData);
+    res.status(201).json({ success: true, data: item });
   } catch (err) {
     next(err);
   }
 };
 
-
-
-
-
-exports.getAll = (req, res) => {
-  const page = parseInt(req.query.page) || 1;
-  const limit = parseInt(req.query.limit) || 10;
-
-  const result = service.getAll(page, limit);
-  res.json(result);
-};
-
-
-exports.getOne = (req, res, next) => {
+exports.getAll = async (req, res, next) => {
   try {
-    const item = service.getOne(req.params.id);
-    res.json(item);
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
 
+    const result = await service.getAll(page, limit);
+    res.json({ success: true, data: result });
   } catch (err) {
     next(err);
   }
 };
 
-exports.update = (req, res, next) => {
+exports.getOne = async (req, res, next) => {
   try {
-    const item = service.update(req.params.id, req.body);
-    res.json(item);
+    const item = await service.getOne(req.params.id);
+    res.json({ success: true, data: item });
   } catch (err) {
     next(err);
   }
 };
 
-exports.remove = (req, res) => {
-  service.remove(req.params.id);
-  res.json({ message: 'Deleted successfully' });
+exports.update = async (req, res, next) => {
+  try {
+    const item = await service.update(req.params.id, req.body);
+    res.json({ success: true, data: item });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.remove = async (req, res, next) => {
+  try {
+    await service.remove(req.params.id);
+    res.status(204).send();
+  } catch (err) {
+    next(err);
+  }
 };
