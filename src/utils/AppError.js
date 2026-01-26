9b3b3f9255b498d0b8ebaae2a850ce7802c73b71
YASWANTH <yaswanthrayana91221@yahoo.com>
2026-01-25 22:17:20 -0500

    Refactor AppError class to set default status code

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -1,6 +1,8 @@
-export default class AppError extends Error {
-    constructor(message, statusCode) {
+class AppError extends Error {
+  constructor(message, statusCode = 500) {
     super(message);
     this.statusCode = statusCode;
-    }
+  }
 }
+
+module.exports = AppError;
